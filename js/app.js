797c3b1ef4f300376a4b1f34a63ba0ad76ba7e00
keroloussamy <keroloussamy98@gmail.com>
2021-11-08 19:55:01 +0200

    Add animation to close button

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 const btnMobile = document.querySelector("#btnmobile");
 const menuMobile= document.querySelector("#menu-mobile");
+const closeMenuBtn = document.querySelector(".close-menu");
 
 btnMobile.addEventListener("click", showUp);
+closeMenuBtn.addEventListener("click", closeUp);
 
 function showUp(){
     menuMobile.classList.add("animate__backInDown","showUp")
+}
+
+function closeUp(){
+    menuMobile.classList.add("animate__fadeOutUp")
+    menuMobile.addEventListener("animationend", removeClasses)
+}
+
+function removeClasses(){
+    menuMobile.classList.remove("animate__backInDown","showUp")
+    menuMobile.classList.remove("animate__fadeOutUp")
 }
