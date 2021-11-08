1ebda46e21527b7f0801db0d1969a8c6b7095d1b
iwillteixeira <iwill2005@gmail.com>
2021-11-08 16:57:11 -0300

    Add scroll to menu-mobile items

--- js/app.js
+++ js/app.js
@@ -1,20 +1,39 @@
 const btnMobile = document.querySelector("#btnmobile");
 const menuMobile= document.querySelector("#menu-mobile");
 const closeMenuBtn = document.querySelector(".close-menu");
+const menuItems = document.querySelectorAll(".mobile-menu-items  a");
 
-btnMobile.addEventListener("click", showUp);
-closeMenuBtn.addEventListener("click", closeUp);
 
-function showUp(){
-    menuMobile.classList.add("animate__backInDown","showUp")
+for(let i of menuItems){
+    
+    i.addEventListener("click", function(e){
+        e.preventDefault();
+        linkItems = i.getAttribute("href");
+        closeUp(e)
+        document.getElementById(linkItems).scrollIntoView({
+            behavior:"smooth"
+        })
+        
+
+    });
+        
+
+        
+}
+
+btnMobile.addEventListener("click", function(a){
+    showUp(a);
+});
+
+
+function showUp(e){
+    e.preventDefault();
+    closeMenuBtn.addEventListener("click", closeUp);
+    menuMobile.classList.add("animate__backInDown","showUp") ;
 }
 
 function closeUp(){
-    menuMobile.classList.add("animate__fadeOutUp")
-    menuMobile.addEventListener("animationend", removeClasses)
+    menuMobile.classList.add("animate__fadeOutUp");
+    menuMobile.classList.remove("animate__backInDown","showUp", "animate__fadeOutUp");
 }
 
-function removeClasses(){
-    menuMobile.classList.remove("animate__backInDown","showUp")
-    menuMobile.classList.remove("animate__fadeOutUp")
-}
