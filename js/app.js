c47db703b97262a27d7c9848ebdeac25658637ca
iwillteixeira <iwill2005@gmail.com>
2021-11-09 10:54:49 -0300

    app.js algo improvement

--- js/app.js
+++ js/app.js
@@ -2,19 +2,28 @@ const btnMobile = document.querySelector('#btnmobile');
 const menuMobile = document.querySelector('#menu-mobile');
 const closeMenuBtn = document.querySelector('.close-menu');
 const menuItems = document.querySelectorAll('.mobile-menu-items > a');
+const menuItemsDesk = document.querySelectorAll(".nav-desk-li > a")
 
 function closeUp() {
   menuMobile.classList.add('animate__fadeOutUp');
   menuMobile.classList.remove('animate__backInDown', 'showUp', 'animate__fadeOutUp');
 }
 
-function closeItems(e) {
-  e.preventDefault();
-  const linkItems = this.getAttribute('href');
-  closeUp(e);
+function scrollTosection(e){
+  console.log(e)
+  const linkItems = e.getAttribute('href');
   document.getElementById(linkItems).scrollIntoView({
     behavior: 'smooth',
   });
+}
+
+function closeItems(e) {
+  
+  let obj = this;
+  
+  closeUp(obj);
+  scrollTosection(obj)
+  
 }
 
 function showUp(e) {
@@ -24,6 +33,10 @@ function showUp(e) {
     menuItems[i].addEventListener('click', closeItems);
   }
   menuMobile.classList.add('animate__backInDown', 'showUp');
+}
+
+for (let i = 0; i < menuItems.length; i += 1) {
+  menuItemsDesk[i].addEventListener('click', closeItems);
 }
 
 btnMobile.addEventListener('click', (a) => {
