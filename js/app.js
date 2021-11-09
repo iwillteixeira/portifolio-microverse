ef0251430ef0e4dc0850ec0910103af3b7c75eef
iwillteixeira <iwill2005@gmail.com>
2021-11-09 16:34:00 -0300

    Add details modal js and css

--- js/app.js
+++ js/app.js
@@ -2,43 +2,48 @@ const btnMobile = document.querySelector('#btnmobile');
 const menuMobile = document.querySelector('#menu-mobile');
 const closeMenuBtn = document.querySelector('.close-menu');
 const menuItems = document.querySelectorAll('.mobile-menu-items > a');
-const menuItemsDesk = document.querySelectorAll(".nav-desk-li > a")
+const menuItemsDesk = document.querySelectorAll('.nav-desk-li > a');
+const detailsbutton = document.querySelectorAll('.work-btn a');
 
 function closeUp() {
   menuMobile.classList.add('animate__fadeOutUp');
   menuMobile.classList.remove('animate__backInDown', 'showUp', 'animate__fadeOutUp');
 }
 
-function scrollTosection(e){
-  console.log(e)
+function scrollTosection(e) {
   const linkItems = e.getAttribute('href');
   document.getElementById(linkItems).scrollIntoView({
     behavior: 'smooth',
   });
 }
 
-function closeItems(e) {
-  
-  let obj = this;
-  
+function closeItems() {
+  const obj = this;
   closeUp(obj);
-  scrollTosection(obj)
-  
+  scrollTosection(obj);
 }
 
 function showUp(e) {
   e.preventDefault();
+  const getEle = this.getAttribute('href');
+  const eleItem = document.querySelector(getEle);
   closeMenuBtn.addEventListener('click', closeUp);
   for (let i = 0; i < menuItems.length; i += 1) {
     menuItems[i].addEventListener('click', closeItems);
   }
-  menuMobile.classList.add('animate__backInDown', 'showUp');
+  eleItem.classList.add('animate__backInDown', 'showUp');
+  const eleItemClose = eleItem.querySelector('.close-modal-details');
+  eleItemClose.addEventListener('click', () => {
+    eleItem.classList.remove('animate__backInDown', 'showUp', 'animate__fadeOutUp');
+  });
+}
+
+for (let i = 0; i < detailsbutton.length; i += 1) {
+  detailsbutton[i].addEventListener('click', showUp);
 }
 
 for (let i = 0; i < menuItems.length; i += 1) {
   menuItemsDesk[i].addEventListener('click', closeItems);
 }
 
-btnMobile.addEventListener('click', (a) => {
-  showUp(a);
-});
+btnMobile.addEventListener('click', showUp);
