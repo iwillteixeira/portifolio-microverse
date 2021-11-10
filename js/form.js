406369e67af078cde05dae045d9c809e65269eef
keroloussamy <keroloussamy98@gmail.com>
2021-11-10 18:26:34 +0200

    Add style to error message, fix linters

--- js/form.js
+++ js/form.js
@@ -1,19 +1,16 @@
-const form = document.getElementById("form");
-const name = document.getElementById("name_form");
-const email = document.getElementById("email_form");
-const message = document.getElementById("message");
-const error = document.querySelector(".error-email")
-const regex = /^[a-z0-9_-]+@[a-z0-9]+\.[a-z]+\.?[a-z]+/g
+const form = document.getElementById('form');
+const email = document.getElementById('email_form');
+const error = document.querySelector('.error-email');
+const regex = /^[a-z0-9_-]+@[a-z0-9]+\.[a-z]+\.?[a-z]+/g;
 
-form.addEventListener("submit", (e)=>{
-    if(regex.test(email.value)){
-        error.textContent = ""
-        email.style.border = '1px solid #cfd8dc';
-    }
-    else{
-        e.preventDefault()
-        email.style.border = '3px solid red';
-        error.textContent = 'nooooo.'
-
-    }
+form.addEventListener('submit', (e) => {
+  if (regex.test(email.value)) {
+    error.textContent = '';
+    email.style.border = '1px solid #cfd8dc';
+  } else {
+    e.preventDefault();
+    email.style.border = '3px solid red';
+    error.textContent = 'The email should be lowercase.';
+    error.style.color = 'red';
+  }
 });
