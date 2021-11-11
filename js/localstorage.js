8a82cad3703fe2f2ce81aa707bae519fea18ec4b
iwillteixeira <iwill2005@gmail.com>
2021-11-11 15:43:03 -0300

    Add localStorage js

---
+++ js/localstorage.js
@@ -0,0 +1,40 @@
+
+    const valuesForm = (e) => {
+      console.log(e.target.value, e.target.name)
+      
+      const formData = {
+        name: form.name.value,
+        email: form.email.value,
+        message: form.message.value,
+      };
+  
+      localStorage.setItem('formData', JSON.stringify(formData));
+    };
+  
+    form.name.addEventListener('change', valuesForm);
+    form.email.addEventListener('change', valuesForm);
+    form.message.addEventListener('change', valuesForm);
+  
+    const existLocal = () => {
+      let name = '';
+      let email = '';
+      let message = '';
+  
+      if (JSON.parse(localStorage.getItem('formData')) === null) {
+        name = '';
+        email = '';
+        message = '';
+      } else {
+        ({ name, email, message } = JSON.parse(localStorage.getItem('formData')));
+      }
+  
+      if (name !== 'empty' || email !== 'empty' || message !== 'empty') {
+        form.name.value = name;
+        form.email.value = email;
+        form.message.value = message;
+      }
+    };
+  
+    document.addEventListener('DOMContentLoaded', () => {
+      existLocal();
+    });
