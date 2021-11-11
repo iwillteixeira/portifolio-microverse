720aaa7a66f4db0af48f2168a08b6fd4f7e48788
keroloussamy <keroloussamy98@gmail.com>
2021-11-11 21:22:08 +0200

    Add onload event and Fix linters

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,41 +1,39 @@
-
-    const valuesForm = (e) => {
-     
-      
-      const formData = {
-        name: form.name.value,
-        email: form.email.value,
-        message: form.message.value,
-      };
-
-      
-  
-      localStorage.setItem('formData', JSON.stringify(formData));
+window.onload = () => {
+  const form = document.getElementById('form');
+  const valuesForm = () => {
+    const formData = {
+      name: form.name.value,
+      email: form.email.value,
+      message: form.message.value,
     };
-  
-    form.name.addEventListener('change', valuesForm);
-    form.email.addEventListener('change', valuesForm);
-    form.message.addEventListener('change', valuesForm);
-  
-    const existLocal = () => {
-      let name = '';
-      let email = '';
-      let message = '';
-  
-      if (JSON.parse(localStorage.getItem('formData')) === null) {
-        name = '';
-        email = '';
-        message = '';
-      } else {
-        ({ name, email, message } = JSON.parse(localStorage.getItem('formData')));
-      }
-  
-      if (name !== 'empty' || email !== 'empty' || message !== 'empty') {
-        form.name.value = name;
-        form.email.value = email;
-        form.message.value = message;
-      }
-    };
-    document.addEventListener('DOMContentLoaded', () => {
-      existLocal();
-    });
+
+    localStorage.setItem('formData', JSON.stringify(formData));
+  };
+
+  form.name.addEventListener('change', valuesForm);
+  form.email.addEventListener('change', valuesForm);
+  form.message.addEventListener('change', valuesForm);
+
+  const existLocal = () => {
+    let name = '';
+    let email = '';
+    let message = '';
+
+    if (JSON.parse(localStorage.getItem('formData')) === null) {
+      name = '';
+      email = '';
+      message = '';
+    } else {
+      ({ name, email, message } = JSON.parse(localStorage.getItem('formData')));
+    }
+
+    if (name !== 'empty' || email !== 'empty' || message !== 'empty') {
+      form.name.value = name;
+      form.email.value = email;
+      form.message.value = message;
+    }
+  };
+  document.addEventListener('DOMContentLoaded', () => {
+    existLocal();
+  });
+};
